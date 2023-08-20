61220c62192cadf0042107218893d6ec8d74b645
Emmanuel <emmanuelmark501@gmail.com>
2023-08-20 07:47:11 -0700

    character conversion specifier

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("Let's print a simple sentence.\n");
+	_printf("%c", 'S');
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	_printf(NULL);
+	_printf("%c", '\0');
+	_printf("%");
+
+	return (0);
+}
--- printf.c
+++ printf.c
@@ -36,6 +36,7 @@ int _printf(const char *format, ...)
 				count += specifier(arg, format);
 			}
 		}
+		format++;
 	}
 
 	return (count);
