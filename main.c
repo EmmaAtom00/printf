424b8f025a7fd4b6cb2b7bf26efa5b9e844ea62f
Emmanuel <emmanuelmark501@gmail.com>
2023-08-20 06:55:13 -0700

    checker

--- main.c
+++ main.c
@@ -13,7 +13,9 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
+    char *str = "I am a new born in christ, therfore i am free indeed";
 
+    _printf("%s", str);
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
